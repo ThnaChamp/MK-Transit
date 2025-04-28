6bd06a0581b63d9dacd4306d92e99730a544be00
[ThnaChamp] <154907300+ThnaChamp@users.noreply.github.com>
2025-04-28 09:25:33 +0700

    Update PathFinder.java

--- app/src/main/java/mktransit/PathFinder.java
+++ app/src/main/java/mktransit/PathFinder.java
@@ -1,6 +1,9 @@
 package mktransit;
 
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class PathFinder {
     private Map<String, Station> stationMap;
@@ -10,11 +13,25 @@ public class PathFinder {
     }
 
     public PathResult findShortestPath(String startId, String endId) {
+
+        // case sensitive
         startId = startId.toUpperCase();
         endId = endId.toUpperCase();
 
-        
+        Map<String, Integer> times = new HashMap<>(); // เก็บเวลาจาก Id ปัจจุบัน -> Id อื่นๆ
+        Map<String, String> previous = new HashMap<>(); // เก็บ Id ก่อนหน้า Id ปัจจุบัน PriorityQueue<Node> queue = new
+        PriorityQueue<Node> queue = new PriorityQueue<>(Comparator.comparingInt(node -> node.time)); //
         // TODO: ยังไม่ได้ทำอะไร
         return null;
+    }
+
+    private static class Node {
+        String id;
+        int time;
+
+        Node(String id, int time) {
+            this.id = id;
+            this.time = time;
+        }
     }
 }
