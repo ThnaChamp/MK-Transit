b0176c599e1262a72cf5c0498d60bd6fbdbfb0d1
Sun888888888 <149927098+Sun888888888@users.noreply.github.com>
2025-04-29 19:08:11 +0700

    lil

---
+++ app/src/main/java/mktransit/StationUtil.java
@@ -0,0 +1,21 @@
+package mktransit;
+import java.util.*;
+
+
+
+public class StationUtil {
+    private Map<String, String> idToNameMap;
+
+    // Constructor รับ list สถานีมาแล้วสร้าง map
+    public StationUtil(List<Station> stations) {
+        idToNameMap = new HashMap<>();
+        for (Station station : stations) {
+            idToNameMap.put(station.getId(), station.getName());
+        }
+    }
+
+    // ฟังก์ชันแปลง ID -> Name
+    public String IDtoName(String id) {
+        return idToNameMap.getOrDefault(id, "Unknown Station");
+    }
+}
